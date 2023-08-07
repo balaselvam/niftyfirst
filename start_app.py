f94aaa47725373eba7e916a29f1c21f1cc7726fe
balaselvam <balaselvam.a@gmail.com>
2023-08-07 10:44:11 +0530

    Update start_app.py

--- start_app.py
+++ start_app.py
@@ -18,12 +18,18 @@ async def startup_event():
         url = "https://groww.in/v1/api/option_chain_service/v1/option_chain/nifty?expiry=2023-08-10"  # Replace this URL with the data source URL
         url_next = "https://groww.in/v1/api/option_chain_service/v1/option_chain/nifty?expiry=2023-08-17"
         url_far = "https://groww.in/v1/api/option_chain_service/v1/option_chain/nifty?expiry=2023-08-31"
+        url_far1 = "https://groww.in/v1/api/option_chain_service/v1/option_chain/nifty?expiry=2023-09-28"
+        url_far2 = "https://groww.in/v1/api/option_chain_service/v1/option_chain/nifty?expiry=2023-12-28"
         filename = "dw_data.json"  # Choose the desired filename for the saved data
         filename_next = "dw_data_next.json"
         filename_far = "dw_data_far.json"
+        filename_far1 = "dw_data_far1.json"
+        filename_far2 = "dw_data_far2.json"
         asyncio.create_task(fetch.fetch_and_save_data(url, filename))
         asyncio.create_task(fetch.fetch_and_save_data_next(url_next, filename_next))
         asyncio.create_task(fetch.fetch_and_save_data_far(url_far, filename_far))
+        asyncio.create_task(fetch.fetch_and_save_data_far1(url_far1, filename_far1))
+        asyncio.create_task(fetch.fetch_and_save_data_far2(url_far2, filename_far2))
     else: 
         print ("Market closed !")
 
